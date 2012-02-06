3fd11b19599dc52570f34968959f360aa36f1bb0
Josh West <jayscribbs@gmail.com>
2012-02-05 22:27:39 -0500

    Setting up git

---
+++ config.h
@@ -0,0 +1,20 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "jayscribbs@gmail.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "GTCue"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "GTCue 0.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "GTCue"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.0"
