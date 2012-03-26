bb638701ce63c0e2f2e0bfe19e32c69bed85fd62
Josh West <jayscribbs@gmail.com>
2012-03-25 22:21:16 -0400

    oops

---
+++ src/Gui/MainWindow.cpp
@@ -0,0 +1,10 @@
+// MainWindow class definition
+
+#include"MainWindow.h"
+
+MainWindow::MainWindow () {
+	set_title("GT-10 Backing Track Cue");
+	set_default_size(640, 480);
+}
+
+MainWindow::~MainWindow () {}
